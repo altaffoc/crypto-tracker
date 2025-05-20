68696e89133cdb852c6f973f362888eda44a03c3
altaffoc <136885537+altaffoc@users.noreply.github.com>
2025-05-20 23:08:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 import os
+from flask import send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/adsrock.txt')
+def serve_adsrock():
+    return send_from_directory('static', 'adsrock.txt')
+    
 @app.route('/')
 def home():
     return render_template('index.html')  # Pastikan file ini ada di folder `templates`
