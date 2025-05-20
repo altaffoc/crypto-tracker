1cb52f1442cd7bebd518f8f38784fac33e7aa1e9
altaffoc <136885537+altaffoc@users.noreply.github.com>
2025-05-20 22:38:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
-import requests
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('index.html')  # Pastikan file ini ada di folder `templates`
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
